37d5306a2432b104d52e66519df567b48018ae5d
gwen <121168261+gwennnnnnnnnn@users.noreply.github.com>
2025-03-18 11:05:33 +0800

    first commit

---
+++ hello.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body{
+            background-color: aquamarine;
+        }
+        h1{
+            color: blue;
+        }
+    </style>
+</head>
+<body>
+    <h1 align="center">HELLO WORLD</h1>
+</body>
+</html>
